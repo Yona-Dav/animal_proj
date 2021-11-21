efe5967c3d9ccf9aba70c5ec84b1d940ab7047a4
Yona-Dav <yona.bohbot@gmail.com>
2021-11-21 22:59:52 +0200

    img added

--- info/urls.py
+++ info/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+# from .views import redirect_view
 
 urlpatterns = [
-    path('animal/<int:animal_id>', views.animal),
-    path('family/<int:family_id>', views.family),
+    path('animal/<int:animal_id>/', views.animal),
+    path('family/<int:family_id>/', views.family),
+    path('animals/', views.animals)
 ]
--- info/views.py
+++ info/views.py
@@ -1,35 +1,46 @@
 from django.shortcuts import render
 import json
-from collections import defaultdict
+from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
+
 # Create your views here.
+def get_data():
+    with open('data.json') as f:
+        return json.load(f)
 
 def animal(request,animal_id):
-    with open('data.json') as f:
-        data = json.load(f)
+    data = get_data()
     selected_animal = None
+    selected_image = None
     for animal in data['animals']:
-        if animal['id']== animal_id:
+        if animal['id'] == animal_id:
             selected_animal = animal
-    return render(request, 'animal.html', {'animal':selected_animal})
+            selected_image = animal['image']
+    return render(request, 'animal.html', {'animal':selected_animal, 'image':selected_image})
 
 def family(request, family_id):
-    with open('data.json') as f:
-        data = json.load(f)
-        selected_family =None
-        list_animal = {}
-        for family in data['families']:
-            if family['id'] == family_id:
-                selected_family = family['name']
+    data = get_data()
+    selected_family =None
+    list_animal = {}
+    for family in data['families']:
+        if family['id'] == family_id:
+            selected_family = family['name']
 
-        for animal in data['animals']:
-            if animal['family'] == family_id:
-                if family_id not in list_animal:
-                    list_animal[family_id] = animal['name']
-                else:
-                    list_animal[family_id] += animal['name']+""
+    for animal in data['animals']:
+        if animal['family'] == family_id:
+            if family_id not in list_animal:
+                list_animal[family_id] = animal['name']
+            else:
+                list_animal[family_id] += animal['name']+""
 
     context = {'animal':list_animal, 'family':selected_family}
     return render(request, 'family.html', context)
 
+def animals(request):
+    data = get_data()
+    animals = {}
+    for animal in data['animals']:
+        animals[animal['id']]= animal['name']
+    return render(request, 'animals.html', {'animals': animals})
 
 
